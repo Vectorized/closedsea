89a18859333afe8f2ea2f535a8ed35574cb2f4c5
Vectorized <webby1111@hotmail.com>
2022-11-15 06:14:22 +0000

    Edit comment

--- src/OperatorFilterer.sol
+++ src/OperatorFilterer.sol
@@ -88,7 +88,6 @@ abstract contract OperatorFilterer {
                     // We'll skip checking if `from` is inside the blacklist.
                     // Even though that can block transferring out of wrapper contracts,
                     // we don't want tokens to be stuck.
-                    // OpenSea's original repo is planning to remove the `from` check.
 
                     // Restore the part of the free memory pointer that was overwritten,
                     // which is guaranteed to be zero, if less than 8tb of memory is used.
