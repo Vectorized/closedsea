498c97246dffa2a2409de484b3fcc9808cfd7726
Vectorized <webby1111@hotmail.com>
2022-11-18 18:00:51 +0000

    Add priority filter functionality

---
+++ test/helpers/PriorityFilterer.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {OperatorFilterer} from "../../src/OperatorFilterer.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {IOperatorFilterRegistry} from "operator-filter-registry/IOperatorFilterRegistry.sol";
+
+contract PriorityFilterer is OperatorFilterer, Ownable {
+    address private immutable _priorityOperator;
+
+    constructor(address priorityOperator) {
+        _priorityOperator = priorityOperator;
+        _registerForOperatorFiltering(address(0), false);
+    }
+
+    function filter(address from) public view onlyAllowedOperator(from, true) returns (bool) {
+        return true;
+    }
+
+    function _isPriorityOperator(address operator) internal view override returns (bool) {
+        return operator == _priorityOperator;
+    }
+}
