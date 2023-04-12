3b29156136ed2710bc1e6bcc55acf53a01349f8a
cygaar <tmpftl1@gmail.com>
2023-04-11 20:51:40 -0700

    Simplify ERC721A example

--- src/example/ExampleERC721A.sol
+++ src/example/ExampleERC721A.sol
@@ -50,6 +50,10 @@ abstract contract ExampleERC721A is
         super.approve(operator, tokenId);
     }
 
+    /**
+     * @dev Both safeTransferFrom functions in ERC721A call this function
+     * so we don't need to override them.
+     */
     function transferFrom(address from, address to, uint256 tokenId)
         public
         payable
@@ -57,24 +61,6 @@ abstract contract ExampleERC721A is
         onlyAllowedOperator(from)
     {
         super.transferFrom(from, to, tokenId);
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId)
-        public
-        payable
-        override (IERC721A, ERC721A)
-        onlyAllowedOperator(from)
-    {
-        super.safeTransferFrom(from, to, tokenId);
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data)
-        public
-        payable
-        override (IERC721A, ERC721A)
-        onlyAllowedOperator(from)
-    {
-        super.safeTransferFrom(from, to, tokenId, data);
     }
 
     function supportsInterface(bytes4 interfaceId)
